53c7c35adc6ee38a05b15513e3ac23ab2390b8df
Vieira <joaovieira@hotmail.com.br>
2022-11-21 21:52:25 -0300

    Atualizacao do main.js

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,6 +1,10 @@
 
 
-const btnTeste = document.getElementById("btnTeste");
-btnTeste.addEventListener("click", function () {
-    console.log('Clicou no botão.')
-})
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
